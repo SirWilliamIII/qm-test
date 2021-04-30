3a7488021baf63acbd3a2419f9d66ce9af7f93e9
Will Carpenter <willcarpenterwastaken@gmail.com>
2021-04-30 11:20:53 -0600

    small changes

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -2,13 +2,13 @@
   <footer>
     <div class="container">
       <router-link class="logo-font" :to="{ name: 'home', params: {} }">
-        conduit
+        the collective brain
       </router-link>
       <span class="attribution">
-        An interactive learning project from
+        I totally stole this from the folks at 
         <a rel="noopener noreferrer" target="blank" href="https://thinkster.io"
           >Thinkster</a
-        >. Code &amp; design licensed under MIT.
+        >. Code &amp; MIT licensed.
       </span>
     </div>
   </footer>
